1159407da5333341e4ca92ae0de602c6f8cfdbe3
Rick Ansay <rick.ansay@gmail.com>
2021-12-26 19:23:19 -0700

    Added all license badging and links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  console.log(`![badge](https://img.shields.io/badge/license-${license}-brightgreen)`);
-  
-  return `![badge](https://img.shields.io/badge/license-${license}-brightgreen)`;
+  if (license === "GNU AGPLv3") {
+      return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+  } else if (license === "GNU GPLv3") {
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "GNU LGPLv3") {
+      return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+  } else if (license === "Mozilla") {
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  } else if (license === "MIT") {
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "Apache") {
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else {
+      return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+  }
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -35,7 +39,7 @@ ${renderLicenseBadge(data.license)}
 ## Usage
 💻 ${data.usage}
 ## License
-![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+${renderLicenseBadge(data.license)}
 <br />
 This application is covered by the ${data.license} license. 
 ## Contributing
