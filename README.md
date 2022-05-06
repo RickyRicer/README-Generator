fca9649d0b5147ef071efeeccf9a39e5a39d7c04
Rick Ansay <rick.ansay@gmail.com>
2022-05-05 22:48:16 -0600

    Final push

--- README.md
+++ README.md
@@ -1,35 +1,66 @@
-# New Readme 👋
+# README Generator 👋
  
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 ## Table of Contents
-- [Description](#description)
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
 ## Description
-🔍 This is a readme generator
+
+This CLI app was created to help you make README files with ease!
+
 ## Installation
-💾 NPM Install
+
+1. Clone the repo
+```sh
+git clone https://github.com/your_username_/Project-Name.git
+```
+2. Install NPM packages
+```sh
+npm install
+```
+3. Run index.js via Node
+```sh
+node ./index.js
+```
+
 ## Usage
-💻 
+
+View the code and try for yourself!
+
+[![Product Name Screen Shot][product-screenshot]](#)
+
 ## License
+
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-<br />
+
 This application is covered by the MIT license. 
+
 ## Contributing
-👪 
+
+
+
+You can contribute to this project by adhering to the code of conduct as mentioned: [here](https://www.contributor-covenant.org/#:~:text=Contributor%20Covenant%20is%20a%20code,that%20distinguish%20your%20own%20community)
+
 ## Tests
-✏️ 
+
+No testing is provided with this suite.
+
 ## Questions
+
 ✋ If you have any questions, please contact me through email or github, listed below:<br />
 <br />
-Find me on GitHub: [rickyricer](https://github.com/rickyricer)<br />
+Find me on GitHub: [RickyRicer](https://github.com/RickyRicer)<br />
 <br />
 ✉️ Email me with any questions: rick.ansay@gmail.com<br /><br />
-🖖_This README was generated with ❤️ by [README-generator](https://github.com/rickyricer/README-Generator) 🔥🔥🔥_
+🖖 This README was generated with ❤️ by [README-generator](https://github.com/RickyRicer/README-Generator) 🔥🔥🔥
     
-  
+<!-- MARKDOWN LINKS & IMAGES -->
+[product-screenshot]: assets/demo.gif
--- index.js
+++ index.js
@@ -1,94 +1,124 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
-const fs = require('fs');
-const path = require('path');
-// TODO: Create an array of questions for user input
+// Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const path = require("path");
+// Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        message: 'What is the title of your readme?',
-        name: 'projectTitle',
-      },
-      // {
-      //   type: 'input',
-      //   message: 'Create a table of contents:',
-      //   name: 'tableOfContent',
-      // },
-      {
-        type: 'input',
-        message: `Provide a short description explaining the what, why, and how of your project.`,
-        name: 'description',
-      },
-      {
-        type: 'input',
-        message: 'What steps are required to install your project?',
-        name: 'installation',
-      },
-      {
-        type: 'input',
-        message: 'Provide instructions and examples for use. Include screenshots as needed.',
-        name: 'usage',
-      },
-      {
-        type: 'input',
-        message: 'Placeholder for contributing',
-        name: 'contributing',
-      },
-      {
-        type: 'input',
-        message: 'Provide information about testing performed for your project.',
-        name: 'tests',
-      },
-      {
-        type: 'input',
-        message: 'Provide email address',
-        name: 'email',
-      },
-      {
-        type: 'input',
-        message: 'Provide your github username',
-        name: 'github',
-      },
-      {
-        type: 'input',
-        message: `List your collaborators, if any, with links to their GitHub profiles.
+  {
+    type: "input",
+    message: "What is the title of your readme?",
+    name: "projectTitle",
+    default () {
+      return `README Generator`
+    }
+  },
+  {
+    type: "input",
+    message: `Provide a short description explaining the what, why, and how of your project.`,
+    name: "description",
+    default () {
+      return `This CLI app was created to help you make README files with ease!`
+    }
+  },
+  {
+    type: "input",
+    message: "What steps are required to install your project?",
+    name: "installation",
+    default () {
+      return `1. Clone the repo
+\`\`\`sh
+git clone https://github.com/your_username_/Project-Name.git
+\`\`\`
+2. Install NPM packages
+\`\`\`sh
+npm install
+\`\`\`
+3. Run index.js via Node
+\`\`\`sh
+node ./index.js
+\`\`\``;
+    }
+  },
+  {
+    type: "input",
+    message:
+      "Provide instructions and examples for use. Include screenshots as needed.",
+    name: "usage",
+    default () {
+      return `View the code and try for yourself!`
+    }
+  },
+  {
+    type: "input",
+    message: "Placeholder for contributing, no input necessary!",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "Provide information about testing performed for your project.",
+    name: "tests",
+    default () {
+      return `No testing is provided with this suite.`
+    }
+  },
+  {
+    type: "input",
+    message: "Provide email address",
+    name: "email",
+    default () {
+      return `rick.ansay@gmail.com`
+    }
+  },
+  {
+    type: "input",
+    message: "Provide your github username",
+    name: "github",
+    default () {
+      return `RickyRicer`
+    }
+  },
+  {
+    type: "input",
+    message: `List your collaborators, if any, with links to their GitHub profiles.
         If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
         If you followed tutorials, include links to those here as well.`,
-        name: 'credits',
-      },
-      {
-        type: 'list',
-        message: 'Select a license:',
-        name: 'license',
-        choices: [ 'GNU AGPLv3', 
-          'GNU GPLv3',
-          'GNU LGPLv3',
-          'Mozilla',
-          'MIT',
-          'Apache',
-          'Open',
-          ]
-      },
+    name: "credits",
+    default () {
+      return ``
+    }
+  },
+  {
+    type: "list",
+    message: "Select a license:",
+    name: "license",
+    choices: [
+      "GNU AGPLv3",
+      "GNU GPLv3",
+      "GNU LGPLv3",
+      "Mozilla",
+      "MIT",
+      "Apache",
+      "Open",
+    ],
+  },
 ];
 
-
-
-// TODO: Create a function to write README file
+//  Create a function to write README file
 function writeToFile(fileName, data) {
   fs.writeFileSync(path.join(process.cwd(), fileName), data);
-   
 }
-// fs.writeFile('README.md', (err) => err ? console.error(err) : console.log('Success!')
-// );
 
-// TODO: Create a function to initialize app
+//  Create a function to initialize app
 function init() {
   inquirer.prompt(questions).then((answers) => {
     console.log(answers);
-    writeToFile('README.md', generateMarkdown({
-       ...answers
-    }));
+    writeToFile(
+      "README.md",
+      generateMarkdown({
+        ...answers,
+      })
+    );
   });
 }
 
